ac4e7717a4118d281f04667d2e2276b9ac9273fc
Fanindra Maharana <imfanindra@gmail.com>
2026-08-14 13:32:09 +0530

    removed umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,6 @@ export default function RootLayout({
     <html lang="en">
       <head></head>
       <body className="antialiased text-sand-12 bg-sand-1 text-[1rem] font-medium selection:bg-[#FFFF05] selection:text-[#000000] min-h-full flex flex-col">
-        <script defer src="https://cloud.umami.is/script.js" data-website-id="f276e1ed-f3c3-4560-aa5c-53e7cf03f6fb"></script>
         <ClickSpark
           sparkColor="#000000"
           sparkSize={10}
